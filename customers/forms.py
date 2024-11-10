5986c2cec451bf343c4767b6f54a446e4b87532d
Light Situma <lightsituma@gmail.com>
2024-11-10 07:58:49 +0300

    modified forms.py and contact.html

--- customers/forms.py
+++ customers/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from customers.models import Customer
 
-class CustomerForm(forms.Form):
+class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
